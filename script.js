47e982d5edad3227a2eb61c357c5c97a437dee73
phurin prasit <phurin@web1.dara.ac.th>
2021-08-05 14:44:33 +0700

    v1

--- script.js
+++ script.js
@@ -3,15 +3,40 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const search = document.querySelector('#search')
+
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  const x = document.querySelector("#author")
-  console.log(x)
+  const y = "640631102 PANNAWIT PANWONG"
+  const x = 640631102+parseInt(length.value)
+  
+  if(btn_toggle.innerHTML == "Show Author"){
+    author.innerHTML = y
+    btn_toggle.innerHTML = "Show Calculation"
+  }else{
+    author.innerHTML = x
+  btn_toggle.innerHTML = "Show Author"
+  }
   //x.innerHTML = "xxx"
-  const h2 = document.createElement('h2')
+  //const h2 = document.createElement('h2')
   //h2.innerHTML = x
 }
-
+search.onclick = () => {
+  var a = document.getElementById("text").innerHTML
+  console.log(a)
+  // var length = document.getElementById("length").value
+  // const myArr = text.split(" ")
+  // var stringOut = ""
+  // for( let i = 0; i < myArr.length; i++){
+  //   if(myArr[i].length >= length){
+  //     stringOut += "<mark>" + myArr[i] + "</mark>"
+  //   }
+  //   else{
+  //     stringOut += myArr[i] + " ";
+  //   }
+  // }
+  // document.getElementById("text").innerHTML = stringOut;
+}
 // more codes for Search and Reset buttons here
